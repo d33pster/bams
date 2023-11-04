9b022234765ae065f03967817df2e93ca124767e
d33pster <deep.main.ac@gmail.com>
2023-11-04 23:47:57 +0530

    login-control

---
+++ control.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+
+    if(!(isset($_SESSION["login"]) && $_SESSION["login"] == "OK")) {
+        header("Location: login.php");
+    }
+    else {
+        header("Location: private.html");
+    }
+?>
