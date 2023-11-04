0ee644e5705fbdcb823490ed4ef05cadb82e9f0e
d33pster <deep.main.ac@gmail.com>
2023-11-05 03:29:37 +0530

    approval

--- approve.php
+++ approve.php
@@ -17,38 +17,8 @@
         $stmt->bind_param("siss", $username, $total, $date, $status);
         $stmt->execute();
         $stmt->close();
-        // get existing loans if any for the user in user_details table
-        $sql = "select loans from users where username='$username';";
-        $result = $conn->query($sql);
-        if ($result->num_rows == 0) {
-            // if no existing loans are present get loan id
-            // of the recently inserted loan record
-            $sql2 = "select loanid from loans where username='$username';";
-            $result2 = $conn->query($sql2);
-            $row2 = $result2->fetch_assoc();
-            $loanAdd = (string)$row2["loanid"]; // recently inserted loan record
-
-            // insert this record in the user_details table
-            $stmt = $conn->prepare("update user_details set loans=? where username=?");
-            $stmt->bind_param("ss", $loanAdd, $username);
-            $stmt->execute();
-            $stmt->close();
-            $conn->close();
-            header("Location: index_loggedin.html");
-        }else{
-            // if existing loans present
-            $sql2 = "select loanid from loans where username='$username';";
-            $result2 = $conn->query($sql2);
-            $row2 = $result2->fetch_assoc();
-            $loanAdd = ", ".$row2["loanid"];
-
-            // insert this record in the user_details table
-            $stmt = $conn->prepare("update user_details set loans=? where username=?");
-            $stmt->bind_param("ss", $loanAdd, $username);
-            $stmt->execute();
-            $stmt->close();
-            $conn->close();
-            header("Location: index_loggedin.html");
-        }
+        $conn->close();
+        // done for now
+        header("Location: index_loggedin.html");
     }
 ?>
